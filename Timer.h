68894bbe782e6dc0b4e17230f7b8071d5b33336e
iadelina <adelina.ivan97@gmail.com>
2018-12-28 22:18:12 +0200

    _BV() Added

--- Timer.h
+++ Timer.h
@@ -10,6 +10,7 @@
 #define TIMER_H_
 
 #include <avr/io.h>
+#define _BV(n) (1<<n)
 
 ISR(TIMER1_COMPA_vect);
 
@@ -25,17 +26,44 @@ void TimerSetup(){
 }
 
 ISR(TIMER1_COMPA_vect){
-	PORTD ^= 0xFC;
-	PORTB ^= 0x1F;
+	//PORTD ^= 0xFC;
+	//PORTB ^= 0x1F;
+
+	PORTD ^= _BV(3);
+	PORTD ^= _BV(4);
+	PORTD ^= _BV(5);
+	PORTD ^= _BV(6);
+	PORTD ^= _BV(7);
+	PORTD ^= _BV(8);
+
+	PORTB ^= _BV(1);
+	PORTB ^= _BV(2);
+	PORTB ^= _BV(3);
+	PORTB ^= _BV(4);
+	PORTB ^= _BV(5);
+	
 }
 
 void Timer(){
-	DDRD=0xFC;//B11111100;
-	DDRB=0x1F;//B11111;
+	//DDRD=0xFC;//B11111100;
+	             
+	//DDRB=0x1F;//B11111;
+	             
+	DDRD |= _BV(3);
+	DDRD |= _BV(4);
+	DDRD |= _BV(5);
+	DDRD |= _BV(6);
+	DDRD |= _BV(7);
+	DDRD |= _BV(8);
+
+	DDRB |= _BV(1);
+	DDRB |= _BV(2);
+	DDRB |= _BV(3);
+	DDRB |= _BV(4);
+	DDRB |= _BV(5);
+
 	TimerSetup();
 }
-
-
 
 
 #endif /* TIMER_H_ */
