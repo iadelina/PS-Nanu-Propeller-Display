9219cab6cc851ecad75c0d155fccb836243daa46
iadelina <adelina.ivan97@gmail.com>
2019-01-06 19:14:53 +0200

    External interrupt config

---
+++ External.h
@@ -0,0 +1,35 @@
+/*
+ * External.h
+ *
+ * Created: 1/2/2019 6:56:05 PM
+ *  Author: Ade
+ */ 
+
+
+#ifndef EXTERNAL_H_
+#define EXTERNAL_H_
+#define _BV(n) (1<<n)
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+void External_interrupt_Setup(void){
+	EICRA |= _BV(ISC00) | _BV(ISC01);  //interrupt on rising edge
+	EIMSK |= _BV(INT0); //enable interrupt on INT0
+	sei();
+}
+
+ISR(INT0_vect){
+	//here goes the hall sensor detection
+
+}
+
+void External(void){
+
+	
+	External_interrupt_Setup();
+}
+
+
+#endif /* EXTERNAL_H_ */
