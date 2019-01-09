d37f0c76a179f876543ee7bb9b4ee18c5d706287
iadelina <adelina.ivan97@gmail.com>
2019-01-09 18:44:29 +0200

    hall sensor detection

--- External.h
+++ External.h
@@ -13,6 +13,10 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+float time_for_one_rot = 0;
+float time_for_one_degree = 0;
+byte last_IN_st;
+unsigned long counter_for_high_state, current_count;
 
 void External_interrupt_Setup(void){
 	EICRA |= _BV(ISC00) | _BV(ISC01);  //interrupt on rising edge
@@ -22,7 +26,19 @@ void External_interrupt_Setup(void){
 
 ISR(INT0_vect){
 	//here goes the hall sensor detection
-
+	current_count = micros();
+	if(PIND & _BV(2)){
+		if(last_IN_st == 0){
+			last_IN_st = 1;
+			counter_for_high_state = current_count;
+		}
+	}
+	else{
+		if(last_IN_st == 1){
+			time_for_one_rot = current_count - counter_for_high_state;
+			time_for_one_degree = time_for_one_rot/360.0;
+		}
+	}
 }
 
 void External(void){
